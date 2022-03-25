61b9ffd826f7bc2574dbe91bd6d575e6dc607ed2
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-03-25 13:47:21 -0700

    Pseudocode - app outline

--- js/script.js
+++ js/script.js
@@ -4,4 +4,27 @@
 
 /////////////////
 
+// Pseudo code: App outline
+
 // Add h1 element with logo (inside of main tag)
+    // add event listener that refreshes the page
+    // center it
+
+// Add "Craft me a Drink" button
+    // center it
+    // add event listener that on the click makes the API call to a random drink
+    // add "crispy click sound" on button click
+
+// Create a Div element to output the data results
+    // Deliver the API data in a well organized and stylized manner
+    // that being:
+    // Drink Title
+    // Image of the drink to the left
+    // Ingredients to the right (Same roll as drink image)
+    // and then finally instructions.
+
+// Optional 
+    // Quote of the Day API
+    // Message about drinking responsably and respecting life.
+
+
