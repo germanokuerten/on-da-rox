886c5a581da4678b1a29684d899f8cd226599f82
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-03-25 13:59:56 -0700

    Pseudocode addition

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 
 // Pseudocode: App outline
 
-// Add h1 element with logo (inside of main tag)
+// Add h1 element with On Da Rox logo (inside of a main tag)
     // add event listener that refreshes the page
     // center it
 
@@ -15,15 +15,14 @@
     // add event listener that on the click makes the API call to a random drink
     // add "crispy click sound" on button click
 
-// Create a Div element to output the data results
+// Create a Div element to output the data results from the API call
     // Deliver the API data in a well organized and stylized manner
     // that being:
     // Drink Title
     // Image of the drink to the left
     // Ingredients to the right (Same roll as drink image)
-    // and then finally instructions.
+    // and then finally recipe instructions
 
 // Optional 
-    // Quote of the Day API
-    // Message about drinking responsably and respecting life.
-    
+    // Quote of the Day API (At the very bottom)
+    // Message about drinking responsibly and respecting life.
