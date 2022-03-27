5ea4ce827f2d5b0f56ec8f439b7a2154097275ad
Germano Kuerten <germanokuerten@Germanos-MBP.lan>
2022-03-26 20:27:59 -0700

    API call added and working, plus button click sound

--- js/script.js
+++ js/script.js
@@ -7,42 +7,51 @@
 // Pseudocode: App outline
 
 // 1. Add header element with On Da Rox logo (inside of a main -> header tag)
-
     // Header element created on html, as well as logo png image.
+    
+    // 1.1 access HTML image element through DOM and crete new variable using jQuery
     const $onDaRoxLogo = $("#ondarox_logo")
 
-    // add event listener that refreshes the page
+    // 1.2 add event listener that refreshes the page
     $onDaRoxLogo.on("click", () => {
         location.reload();
     })
 
-    // center it
+    // 1.3 center it
         // done on css.
 
 // 2. Add "Craft me a Drink" button
+        
+        // done on html and css
 
-    // done on html and css
+    const $drinkButton = $("button")
 
-    const $drinkButton = $("drink_button")
+    // 2.1 center it
+        // done in css through referencing <header> tag/element
 
-    // center it
-        // done in css through <header> tag/element
+    // 2.2 add event listener that on the click makes the API call to a random drink
 
-    // add event listener that on the click makes the API call to a random drink
+    const APIKEY = "9973533";
 
-    
-    
+    $drinkButton.on("click", () => {
+        $.ajax(`https://www.thecocktaildb.com/api/json/v2/${APIKEY}/randomselection.php`)
+        .then((data) => {
+            // test API data
+            console.log(data)
 
-
-    // add "crispy click sound" on button click
-
-// 3. Create a Div element to output the data results from the API call
+    // 2.3 add "crispy click sound" on button click
+        // done on html
+        
+// 3. Create an element to output the data results from the API call
     // Deliver the API data in a well organized and stylized manner
     // that being:
     // Drink Title
     // Image of the drink to the left
     // Ingredients to the right (Same roll as drink image)
     // and then finally recipe instructions
+
+    })
+})
 
 // 4. Optional 
     // Quote of the Day API (At the very bottom)
